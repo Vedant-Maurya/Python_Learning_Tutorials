b49c2cdc1220ceda9a3b8c30f6ff53eef6d46bcd
Vedant-Maurya <vedant7052@gmail.com>
2024-09-25 06:05:13 -0700

    List Details
    In this python files all details of LIST data types ,

---
+++ List_Python.py
@@ -0,0 +1,42 @@
+#List in Python
+
+==> Lists are used to store multiple items in a single variable.
+==> Lists are one of 4 buit-in data types in python used to store collections of data, the other 3 are [ TUPLES , SET , and DICTIONARY ] all with different qualities and usage
+
+==> Lists are the simplest containers that are an integral part of the python language.
+==> A single List may contains DataTypes Like -> Integers,Strings, as well as objects.
+==> Lists are mutable, and hence, they can be altered even after their creation.
+
+==> Store Multiples data in One variables with help of 4-data types => [ List,Tuples,Dictionary,Set ]
+==> List is uses of  # SQUARE BRACKETS [ ] -> Brackets
+==> List items is SEPARATE by COMMAS (,)
+
+ # Functions of List
+ ==> List items are Ordered, Changeable, and allow Duplicate values .
+
+  # Ordered ->
+              when we say that lists are ordered, it means that the items have a defined order, and that order will not change
+               EX => a = [ a,b,c,1,8,9,10 ] -> 1,2,3,4,5,6,7   ( this is the order of list ) 
+
+  # Changeable ->
+              the list is changeable,meaning that we can change, add , and remove items in a list after it has been created.
+  # Allow Duplicates ->
+              since lists are Indexed, lists can have items with the same value.
+              
+========================================================================================================================================================================
+
+                                                                 Indexing in List ->
+
+                                                                 Examples =>
+                                                                                     a  b  c  d  e
+                                                                  Positive index =>  0  1  2  3  4         -> Left to Right
+                                                                  Negative index => -5 -4 -3 -2 -1         -> Right to Left
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+                                                      syntax of list =>          a = [ "apple","banana","cherry" ]
+                                                                                 print(a)
+                                                                
+
+
+                                                                  
