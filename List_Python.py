4c5cd8d3885052b40cc9efc7a286d8ba3d52f499
Vedant-Maurya <vedant7052@gmail.com>
2024-09-25 07:28:18 -0700

    List In Python
    List in python all details of list in this file and this file in only for learning

--- List_Python.py
+++ List_Python.py
@@ -22,7 +22,10 @@
               the list is changeable,meaning that we can change, add , and remove items in a list after it has been created.
   # Allow Duplicates ->
               since lists are Indexed, lists can have items with the same value.
-              
+
+  # LIST Are allows ALL Data types :-
+                             [ string,float, complex,tuple,integers,booleans ] -> by separate the Commas (,)
+                              
 ========================================================================================================================================================================
 
                                                                  Indexing in List ->
@@ -36,7 +39,154 @@
 
                                                       syntax of list =>          a = [ "apple","banana","cherry" ]
                                                                                  print(a)
-                                                                
+                                                                                 print(type(a))
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+                                                                    Allow Duplicates  In LIST :-
+
+                                                                    a = [ "apple","banana","apple","cherry","apple","banana" ]
+                                                                    print(a)
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+                                                                    LIST Allows All Data Types :-
+                                                                                                [ string,float,complex,integers,booleans,tuple,set ]
+                                                                        
+                                                                    Examples  :-
+                                                                            1-> list1 = ["apple","cherry","banana", ]
+                                                                                list2 = [2,5,8,9,7]
+                                                                                list3 = [7.8,8.5,9.5]
+                                                                                list4 = [True,False,True]
+                                                                                print(list1)
+                                                                                print(list2)
+                                                                                print(list3)
+                                                                                print(list4)
+
+                                                                            2-> a = [ 12,-85,20.5,3.6,-98,"students","hello",True,False]
+                                                                                print(a)
+                                                                                print(type(a))
 
 
-                                                                  
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+                                                                            Allows Items In List  ->
+                                                                                        Indexing Items in List :- Left TO Right => Start 0-5
+                                                                                                                  Right To Left => Start -1-5
+                                                                                                                    
+                                                                                        Examples :-
+                                                                                            1-> a=[50,"apple",50.6,5]
+                                                                                                 print(a[2])
+                                            
+                                                                                            2-> b=[50,"banana",50.6]
+                                                                                                print(a[-3])
+
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+                                                                                    Slicing In List :-
+                                                           => Its means a biggest part of thing cut the [ Small parts of area ] this is called the slicing [ slicing ->cutting ] and its used to the
+                                                           => [ Start:End:Step ] ->  [ 0:1:2]
+                                                           => and End is work on the [ n+1 ] Decrement the value 1
+                                                           => and Step is work on the by ( n+1 Increment of 1 value )
+                                                                                   Examples ->
+                                                                                           1-> a = [10,50,60,20,82,52]
+                                                                                               print(a[0:6])       -> its work in by decrement of  1 number
+                                                                                               
+                                                                                           2-> a = [10,50,60,20,82,52]
+                                                                                               print(a[0:6:2])     -> its use the step 2
+
+                                                                                           3-> a = ["python",'java',"css",50,5.6,]
+                                                                                               print(a[1:4])                                                                                        
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+                                                                    Slicing Change Items In Lists ->
+                                                                    
+                                                                           Examples-> 
+                                                                                    1-> a = ["apple","banana","cherry"]
+                                                                                        a[1] = "orange"
+                                                                                        print(a)                ---> "apple","orange","cherry"
+
+
+                                                                                    2-> b = ["orange","pineapple"20,580,58,99,100]
+                                                                                        b[4] = "apple"
+                                                                                        print(b)
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+                                                                     Slicing Change a Range of items values :-
+
+                                                                                      Examples :-
+                                                                                             1->  a = [10,20,50,60,80,90]
+                                                                                                  a[2:5] = ["books","copy","pen"]
+                                                                                                  print(a)       -> 10,20,"books","copy","pen"
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+                                                                    .append,,.remove function in LIST :-
+                                                                    
+                                                                    => .append -> this function is used to (ADD) the items of the list
+                                                                    => .remove -> and this function is used to (REMOVE) the items of the list
+                                                                    => .reverse -> and this function is used to (REVERSE) the items of the list
+
+                                                                    Examples =>
+                                                                            1-> a=[10,20,30,40,50,60,70]
+                                                                                a.append(100)
+                                                                                print(a)
+                                                                                
+                                                                            2-> a=[10,20,30,40,50,60,70]
+                                                                                a.remove(30)
+                                                                                print(a)
+
+                                                                            3-> a=["apple","banana","orange"]
+                                                                                a.append("pineapple")
+                                                                                print(a)
+
+                                                                            4-> a=["apple","banana","orange"]
+                                                                                a.remove("banana")
+                                                                                print(a)
+
+                                                                            5-> a=["apple","banana","orange",20,30,40,50,60]
+                                                                                a.reverse()
+                                                                                print(a)
+
+
+===========================================================================================================================================================================
+
+    #Write the program all list items to add this items in given the lists :-
+
+    l = [10,20,30,40,50,60,70,80,90]
+    print(len(l))
+    a = 0
+    for i in range(9):
+    a = a + l[i]
+    i = i + 1
+    print("the sum of a list is :-",a)
+    
+
+
+                                                                                
+                                                                                
+
+
+
+
+
+                                                                                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
